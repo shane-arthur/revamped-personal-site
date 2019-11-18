d8042a33fe25fd88b87474345014d8091fc78024
Shane Arthur <shane.arthur@gmail.com>
2019-11-17 22:09:49 -0500

    more responsive fixes.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
   showProfession = false;
   constructor() { }
 
   onShowProfession(){
     this.showProfession = true;
+  }
+
+  ngOnInit(){
+    setTimeout(() => {
+      if (!this.showProfession){
+        this.showProfession = true;
+      }
+    }, 5000)
+  }
+
+  checkProfession(){
+
   }
 
 }
