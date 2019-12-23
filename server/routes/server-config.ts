c5c6b504c193fc5f0a6576a3a0dcc99d43170e1e
Shane Arthur <shane.arthur@gmail.com>
2019-12-22 21:35:29 -0500

    Add cors for prod.

--- server/routes/server-config.ts
+++ server/routes/server-config.ts
@@ -1,6 +1,8 @@
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 export default function (app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
 }
