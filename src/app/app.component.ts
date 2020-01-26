3b9796307123ead69e5739a7ef02005aedb83930
Shane Arthur <shane.arthur@gmail.com>
2020-01-25 20:58:19 -0500

    Fix aspect ratio on picture, lazy load first image source.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { ModalService } from './services/modal.service';
+import { of } from 'rxjs';
+import { delay, tap, take } from 'rxjs/operators';
 
 
 @Component({
@@ -7,12 +9,26 @@ import { ModalService } from './services/modal.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   smallPopup = false;
   popupType: string;
-  constructor(private modalService: ModalService){
+  preloadedImageUrl: string;
+  constructor(private modalService: ModalService) {
 
+  }
+
+  ngAfterViewInit() {
+    this._populatePreloadedImages();
+  }
+
+  private _populatePreloadedImages() {
+    const firstPersonalImage = '../assets/images/shane.png';
+    of(['dummyValue']).pipe(
+      delay(0),
+      tap(() => this.preloadedImageUrl = firstPersonalImage),
+      take(1)
+    ).subscribe();
   }
 
   headerItemClicked($event) {
