b8dfc270a80f9e8acf5739fbf7576837ac648e2d
Shane Arthur <shane.arthur@gmail.com>
2020-05-20 14:23:26 -0400

    Intermittent checkin.

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -9,7 +9,7 @@ import { Animations } from './animation';
   animations: [Animations.numberChanged]
 })
 export class ResumeComponent {
-  pdfSrc = '/assets/pdfs/resume5.pdf';
+  pdfSrc = '/assets/pdfs/resume6.pdf';
   currentPage = 1;
   maxPages = 2;
   constructor(private modalService: ModalService) { }
