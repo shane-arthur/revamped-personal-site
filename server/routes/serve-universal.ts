679f029fc744c77e9e4c5702d527c8724ea8cd4d
Shane Arthur <shane.arthur@gmail.com>
2020-07-29 15:05:25 -0400

    Intermittent checkin.

--- server/routes/serve-universal.ts
+++ server/routes/serve-universal.ts
@@ -1,11 +1,9 @@
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../../dist/server/main');
 
 export default function(app){
     app.engine('html', ngExpressEngine({
-        bootstrap: AppServerModuleNgFactory,
-        providers: [provideModuleMap(LAZY_MODULE_MAP)]
+        bootstrap: AppServerModuleNgFactory
     }));
 }
